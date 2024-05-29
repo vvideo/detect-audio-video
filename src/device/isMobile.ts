d10d6f7b2489ad841f1eb9122a43ab2ae1189fad
Denis Seleznev <hcodes@users.noreply.github.com>
2024-05-29 10:32:01 +0300

    Rename getIsMobile helper

--- src/device/isMobile.ts
+++ src/device/isMobile.ts
@@ -1,4 +1,4 @@
 // https://developer.mozilla.org/en-US/docs/Web/HTTP/Browser_detection_using_the_user_agent#Mobile_Tablet_or_Desktop
-export function getIsMobile() {
+export function isMobile() {
     return /Mobi|Android/i.test(window.navigator.userAgent);
 }
