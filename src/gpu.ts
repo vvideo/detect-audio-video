254c5a4b87670812e6535fe6088723376bdaa8b1
Denis Seleznev <hcodes@users.noreply.github.com>
2024-05-29 10:32:41 +0300

    Add hasHardwareAcceleration helper

--- src/gpu.ts
+++ src/gpu.ts
@@ -1,3 +1,6 @@
+import { H264_BASELINE_CONTENT_TYPE } from "./contentTypes/video";
+import { isChromium } from "./device/isChromium";
+
 export function getGpuVendor() {
     const canvas = document.createElement('canvas');
 
@@ -46,4 +49,21 @@ export function getGpuRenderer(): string {
     } catch (e) {}
 
     return '';
+}
+
+export function hasHardwareAcceleration(): Promise<boolean | undefined> {
+    if (!isChromium() || !navigator.mediaCapabilities || !navigator.mediaCapabilities.decodingInfo) {
+        return Promise.resolve(undefined);
+    }
+
+    return navigator.mediaCapabilities.decodingInfo({
+        type: 'file',
+        video: {
+            contentType: H264_BASELINE_CONTENT_TYPE,
+            width: 800,
+            height: 600,
+            bitrate: 100000,
+            framerate: 30,
+        }
+    }).then(result => result.powerEfficient).catch(() => undefined);
 }
