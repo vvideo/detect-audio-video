5aefa7afae43053ea3806823aa185641c01fbc48
Denis Seleznev <hcodes@users.noreply.github.com>
2024-07-28 17:10:22 +0300

    Add isDocumentPipSupported helper

--- src/videoelement.ts
+++ src/videoelement.ts
@@ -15,6 +15,16 @@ export function isPipSupported(): boolean {
     return 'pictureInPictureEnabled' in document || 'webkitPresentationMode' in video;
 }
 
+export function isDocumentPipSupported() {
+    if (typeof window === 'undefined') {
+        return false;
+    }
+
+    // https://developer.mozilla.org/en-US/docs/Web/API/DocumentPictureInPicture
+    // @ts-ignore
+    return Boolean(window.documentPictureInPicture);
+}
+
 export function isCastToAirPlaySupported(): boolean {
     return Boolean(
         typeof window !== 'undefined' &&
