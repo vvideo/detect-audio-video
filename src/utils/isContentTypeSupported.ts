e2dce74c117369ecf9f4c99922e95d9f90d5e3ff
Denis Seleznev <hcodes@users.noreply.github.com>
2024-12-08 15:20:13 +0300

    Fix detection of codecs for old iPad, PS4 and ...

--- src/utils/isContentTypeSupported.ts
+++ src/utils/isContentTypeSupported.ts
@@ -5,11 +5,20 @@ export function isContentTypeSupported(contentType: string) {
     const canPlayTypeResult = canPlayType(contentType);
     const isTypeSupportedResult = isTypeSupported(contentType);
 
+    let any = canPlayTypeResult !== '' || isTypeSupportedResult;
+    let both = canPlayTypeResult !== '' && isTypeSupportedResult;
+
+    // Fix for PS4, old iPad and ...
+    if (!isTypeSupportedResult && canPlayTypeResult === 'maybe') {
+        any = false;
+        both = false;
+    }
+
     return {
         canPlayType: canPlayTypeResult,
         isTypeSupported: isTypeSupportedResult,
-        any: canPlayTypeResult !== '' || isTypeSupportedResult,
-        both: canPlayTypeResult !== '' && isTypeSupportedResult,
+        any,
+        both,
         mediaElementType: contentType.split('/')[0],
         contentType,
     };
