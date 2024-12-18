f27714ef91670170a0cac78e5e2434816b662ca9
Denis Seleznev <hcodes@users.noreply.github.com>
2024-12-18 19:02:58 +0300

    🌀

--- example/build/index.js
+++ example/build/index.js
@@ -235,7 +235,7 @@
 
     function getFirefoxVersion() {
         const match = navigator.userAgent.match(/Firefox\/([0-9]+)\./);
-        return match ? (parseInt(match[1], 10) || -1) : -1;
+        return match ? (parseFloat(match[1]) || -1) : -1;
     }
 
     function isFirefox() {
