61fbc0d4493bf0361d9ea99323fb313ff239ae63
Denis Seleznev <hcodes@users.noreply.github.com>
2024-12-28 21:47:48 +0300

    Fix regexp for PlayStation

--- src/device/isPlayStation.ts
+++ src/device/isPlayStation.ts
@@ -1,19 +1,19 @@
 export function isPlayStationVita() {
-    return /PlayStation Vita/.test(navigator.userAgent);
+    return /PlayStation Vita/i.test(navigator.userAgent);
 }
 
 export function isPlayStation3() {
-    return /PlayStation 3/.test(navigator.userAgent);
+    return /PlayStation 3/i.test(navigator.userAgent);
 }
 
 export function isPlayStation4() {
-    return /PlayStation 4/.test(navigator.userAgent);
+    return /PlayStation 4/i.test(navigator.userAgent);
 }
 
 export function isPlayStation5() {
-    return /PlayStation 5/.test(navigator.userAgent);
+    return /PlayStation 5/i.test(navigator.userAgent);
 }
 
 export function isPlayStation6() {
-    return /PlayStation 6/.test(navigator.userAgent);
+    return /PlayStation 6/i.test(navigator.userAgent);
 }
