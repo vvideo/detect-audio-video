bfa1dcd34878f062646d6eac1724daf7218782f2
Denis Seleznev <hcodes@users.noreply.github.com>
2025-01-06 11:32:28 +0300

    Fix webrtc codecs

--- src/webrtc.ts
+++ src/webrtc.ts
@@ -13,15 +13,23 @@ function mediaFilter(codec: RTCRtpCodec): boolean {
 }
 
 // https://developer.mozilla.org/en-US/docs/Web/Media/Formats/WebRTC_codecs#supported_video_codecs
-export function getWebRTCVideoCodecs(onlyMedia?: boolean): RTCRtpCodec[] {
-    const codecs = window.RTCRtpSender?.getCapabilities('video')?.codecs || [];
+export function getWebRTCVideoCodecs(onlyMedia?: boolean): { encoding: RTCRtpCodec[]; decoding: RTCRtpCodec[]; } {
+    const senderCodecs = window.RTCRtpSender?.getCapabilities('video')?.codecs || [];
+    const receiverCodecs = window.RTCRtpReceiver?.getCapabilities('video')?.codecs || [];
 
-    return onlyMedia ? codecs.filter(mediaFilter) : codecs;
+    return {
+        encoding: onlyMedia ? senderCodecs.filter(mediaFilter) : senderCodecs,
+        decoding: onlyMedia ? receiverCodecs.filter(mediaFilter) : receiverCodecs,
+    };
 };
 
 // https://developer.mozilla.org/en-US/docs/Web/Media/Formats/WebRTC_codecs#supported_audio_codecs
-export function getWebRTCAudioCodecs(onlyMedia?: boolean): RTCRtpCodec[] {
-    const codecs = window.RTCRtpSender?.getCapabilities('audio')?.codecs || [];
+export function getWebRTCAudioCodecs(onlyMedia?: boolean): { encoding: RTCRtpCodec[]; decoding: RTCRtpCodec[]; } {
+    const senderCodecs = window.RTCRtpSender?.getCapabilities('audio')?.codecs || [];
+    const receiverCodecs = window.RTCRtpReceiver?.getCapabilities('audio')?.codecs || [];
 
-    return onlyMedia ? codecs.filter(mediaFilter) : codecs
+    return {
+        encoding: onlyMedia ? senderCodecs.filter(mediaFilter) : senderCodecs,
+        decoding: onlyMedia ? receiverCodecs.filter(mediaFilter) : receiverCodecs,
+    };
 };
