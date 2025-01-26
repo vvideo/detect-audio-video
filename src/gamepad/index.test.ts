e797c783aeb26810a655de619fd5319bf26a3220
Denis Seleznev <hcodes@users.noreply.github.com>
2025-01-26 23:34:40 +0300

    Add DualSense Edge for tests

--- src/gamepad/index.test.ts
+++ src/gamepad/index.test.ts
@@ -2,7 +2,7 @@ import { tests as otherTests } from './test-data/other';
 import { tests as artplayTests } from './test-data/artplay';
 import { tests as ps3DualShock3Tests } from './test-data/ps3-dualshock3';
 import { tests as ps4DualShock4Tests } from './test-data/ps4-dualshock4';
-import { tests as ps5DualSenseTests } from './test-data/other';
+import { tests as ps5DualSenseTests } from './test-data/ps5-dualsense';
 import { tests as speedLinkTests } from './test-data/speedlink-usb';
 import { tests as xboxOneTests } from './test-data/xbox-one';
 
--- src/gamepad/test-data/ps5-dualsense.ts
+++ src/gamepad/test-data/ps5-dualsense.ts
@@ -34,5 +34,38 @@ export const tests: TestExpectedData = [
             vendorId: 1356,
             isStandardGamepad: true
         }
-    ]
+    ],
+    [
+        // MacOS Firefox 133
+        '54c-df2-DualSense Edge Wireless Controller',
+        {
+            name: 'DualSense Edge Wireless Controller',
+            originalName: '54c-df2-DualSense Edge Wireless Controller',
+            originalVendorId: '54c',
+            originalProductId: 'df2',
+            productId: 3570,
+            vendorId: 1356,
+        }
+    ],
+    [
+        // MacOS Chrome 131.0
+        'DualSense Edge Wireless Controller (STANDARD GAMEPAD Vendor: 054c Product: 0df2)',
+        {
+            name: 'DualSense Edge Wireless Controller',
+            originalName: 'DualSense Edge Wireless Controller (STANDARD GAMEPAD Vendor: 054c Product: 0df2)',
+            originalVendorId: '054c',
+            originalProductId: '0df2',
+            productId: 3570,
+            vendorId: 1356,
+            isStandardGamepad: true,
+        }
+    ],
+    [
+        // MacOS Safari 18
+        'DualSense Edge Wireless Controller Extended Gamepad',
+        {
+            name: 'DualSense Edge Wireless Controller Extended Gamepad',
+            originalName: 'DualSense Edge Wireless Controller Extended Gamepad'
+        }
+    ]    
 ];
